a37c4be1c1d75c1f692248fcb2375f052858df36
ajith17bh123 <kkajith321123@gmail.com>
2025-06-29 00:27:24 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,20 @@
+public class Product {
+    private int id;
+    private String name;
+    private String type;
+    private double price;
+    private boolean inStock;
+
+    // Constructors
+    public Product() {}
+    public Product(int id, String name, String type, double price, boolean inStock) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+        this.inStock = inStock;
+    }
+
+    // Getters and Setters
+    // ...
+}
